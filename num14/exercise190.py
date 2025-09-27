de8bd11b2589332b7f4ff72563d56981900e538f
Thalanas110 <amdimate43@gmail.com>
2025-09-27 23:57:25 +0800

    first initial commit

---
+++ num14/exercise190.py
@@ -0,0 +1,14 @@
+#number 14
+# exercise 190 - 4x2 integer array and print its attributes
+import numpy as np
+
+firstArray = np.emmpty([4,2], dtpe = np.uint16)
+
+print(firstArray)
+
+print(firstArray.shape)
+print(firstArray.ndim)
+print(firstArray.itemsize)
+
+
+
---
+++ num14/exercise191.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+my_array = np.arrange(100,200,10)
+my_array = my_array.reshape(5,2)
+print(my_array)
