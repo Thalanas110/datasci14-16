0c8e8ebafb10d30980adeba2eee081473758ef32
Thalanas110 <amdimate43@gmail.com>
2025-09-30 21:06:27 +0800

    hahaha

--- num14/exercise195.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-new_array=np.arange(10,34,1)
-new_array=new_array.reshape(8,3)
-print(new_array)
-# now to divide array into 4 equal division
-subarray=np.split(new_array,4)
-print("="*100)
-print(subarray)
--- num14/exercise196.py
+++
@@ -1,9 +0,0 @@
-sampleArray = np.array([[34,43,73],[82,22,12],[53,94,66]])
-print(sampleArray)
-#sorting original array by second row
-sortArrayByRow = sampleArray[:,sampleArray[1,:].argsort()]
-print("Sorting Original array by secoond row")
-print(sortArrayByRow)
-print("Sorting Original array by second column")
-sortArrayByColumn = sampleArray[sampleArray[:,1].argsort()]
-print(sortArrayByColumn)
